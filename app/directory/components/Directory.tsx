a3e872a8a6d788a1826911df90a2d5967edbc6b0
James Beston <james@beston.me>
2023-11-09 17:02:29 +0000

    Okaaaaaayyyyy…

--- app/directory/components/Directory.tsx
+++ app/directory/components/Directory.tsx
@@ -6,9 +6,7 @@ import Zooplankton from './Zooplankton';
 export default function Directory() {
   return (
     <div className="space-y-6">
-      {/* @ts-expect-error Async Server Component */}
       <Phytoplankton />
-      {/* @ts-expect-error Async Server Component */}
       <Zooplankton />
     </div>
   )
